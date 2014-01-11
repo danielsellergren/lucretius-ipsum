2f044f89926dfbdbbc9c270c90b039743ef97712
Daniel Sellergren <danielsellergren@gmail.com>
2014-01-11 15:48:49 -0500

    Preliminary parsing of English text

--- parse.php
+++ parse.php
@@ -33,6 +33,33 @@
 
 	}
 
-	print_r($latin_passages);
+	// print_r($latin_passages);
+
+	// Open raw file from http://www.gutenberg.org/files/785/785-h/785-h.htm
+	$english_file_name = "./raw/english1_clean.html";
+	$english_file = file_get_contents($english_file_name);
+
+	$english_file_stripped = strip_tags($english_file, '<h2>');
+
+	$english_sections = explode('<h2>', $english_file_stripped);
+
+	$english_passages = array();
+	foreach ($english_sections as $english_section) {
+		$english_section_stripped = trim(implode("\n", array_slice(explode("\n", $english_section), 4)));
+
+		$english_section_passages = explode("\n\n", $english_section_stripped);
+
+		foreach ($english_section_passages as $english_section_passage) {
+			if (trim($english_section_passage) !== "") {
+				$english_passages[] = str_replace('    ', '', trim($english_section_passage));
+			}
+		}
+
+
+
+		// $english_passages[] = trim($english_section_stripped);
+	}
+
+	print_r($english_passages);
 
 ?>
