8ee75ce6bdf3c4bcd7465f45203ae5e6c7034b3c
Daniel Sellergren <danielsellergren@gmail.com>
2014-01-11 11:59:09 -0500

    Cleans up style

---
+++ lucretius.js
@@ -0,0 +1,14 @@
+$(function() {
+
+	$('button#generate').on('click', function() {
+		alert("Generating.");
+	});
+
+// $("a[href^='http']").each(function() {
+//     $(this).css({
+//         background: "url(http://g.etfv.co/" + this.href + ") left center no-repeat",
+//         "padding-left": "50px"
+//     });
+// });
+
+});
