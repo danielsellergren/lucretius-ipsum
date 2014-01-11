3a431ca43e7c21f40a36404a5241a8fd286f403d
Daniel Sellergren <danielsellergren@gmail.com>
2014-01-11 11:17:44 -0500

    Adds jQuery, JS, and CSS

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Lucretius Ipsum</title>
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js" ></script>
+	<script src="lucretius.js"></script>
+	<link type="text/css" rel="stylesheet" href="lucretius.css" />
+</head>
+<body>
+	
+</body>
+</html>
