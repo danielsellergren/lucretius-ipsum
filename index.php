4173cb41aa4322213b9fbde8524ac17fa1079cd9
Daniel Sellergren <danielsellergren@gmail.com>
2014-01-11 11:27:36 -0500

    Builds basic markup structure

--- index.php
+++ index.php
@@ -7,6 +7,20 @@
 	<link type="text/css" rel="stylesheet" href="lucretius.css" />
 </head>
 <body>
-	
+	<div id="container">
+		<header>
+			<h1>Lucretius Ipsum</h1>
+			<h2>Latin/English filler text generator using <em>De rerum natura</em> by Lucretius</h2>
+		</header>
+		<section id="ipsum">
+			<textarea id="latin"></textarea>
+			<textarea id="english"></textarea>
+			<button id="generate">Generate</button>
+		</section>
+		<footer>
+			<span id="provided-by">Text provided by <a href="http://www.thelatinlibrary.com/lucretius/lucretius1.shtml">The Latin Library</a> and <a href="http://www.gutenberg.org/files/785/785-h/785-h.htm">Project Gutenberg</a></span>
+			<span id="credit">Code by <a href="http://danielsellergren.com">Daniel Sellergren</a> | <a href="https://github.com/danielsellergren/lucretius-ipsum">Source Code</a></span>
+		</footer>
+	</div>
 </body>
 </html>
