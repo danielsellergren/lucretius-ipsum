c7c339428c7d439c0360a0911c8ccaf96f6df3d4
Daniel Sellergren <danielsellergren@gmail.com>
2014-01-11 14:04:10 -0500

    Grabs favicons for footer links

--- lucretius.js
+++ lucretius.js
@@ -1,5 +1,6 @@
 $(function() {
 
+	// On Generate click: Call for sample data in JSON
 	$('button#generate').on('click', function() {
 		$.getJSON( "lucretius.json", function(data) {
 			console.log(data);
@@ -7,5 +8,14 @@ $(function() {
 			$('#english').html(data.english);
 		});
 	});
-	
+
+	// Adds favicons for external links
+	$("footer a[href^='http']").each(function() {
+	    $(this).css({
+	        background: "url(http://www.google.com/s2/u/0/favicons?domain=" + this.hostname + 
+	        ") left center no-repeat",
+	        "padding-left": "20px"
+	    });    
+	});
+
 });
