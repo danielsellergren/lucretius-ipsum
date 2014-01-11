f65d28e613a5351023153da4b16f5c38624bf192
Daniel Sellergren <danielsellergren@gmail.com>
2014-01-11 16:25:17 -0500

    Separates English and Latin parsers

---
+++ parse_latin.php
@@ -0,0 +1,31 @@
+<?php 
+
+	// Open raw files separated from http://www.gutenberg.org/files/785/785-h/785-h.htm
+	$english_file_name = "./raw/english1.html";
+	$english_file = file_get_contents($english_file_name);
+
+	$english_file_stripped = strip_tags($english_file, '<h2>');
+
+	$english_sections = explode('<h2>', $english_file_stripped);
+
+	$english_passages = array();
+	foreach ($english_sections as $english_section) {
+		$english_section_stripped = trim(implode("\n", array_slice(explode("\n", $english_section), 8)));
+
+		$english_section_passages = explode("\n\n", $english_section_stripped);
+
+		foreach ($english_section_passages as $english_section_passage) {
+			if (trim($english_section_passage) !== "") {
+				$english_passages[] = str_replace('    ', '', trim($english_section_passage));
+			}
+		}
+
+	}
+
+	$english_json = json_encode($english_passages);
+
+	$english_json_file = fopen('./json/english1.json', 'w');
+	fwrite($english_json_file, $english_json);
+	fclose($english_json_file);
+
+?>
